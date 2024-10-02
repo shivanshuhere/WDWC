946c3bbf95b391e75dd5f8b9e7506f546da5f1d1
Shivanshu Koundal <126767653+shivanshuhere@users.noreply.github.com>
2024-10-02 16:59:04 +0530

    use prettier

--- node/todo/todo.js
+++ node/todo/todo.js
@@ -1,75 +1,64 @@
-const fs = require("fs")
+const fs = require("fs");
 
-let command = process.argv[2]
-let arg = process.argv[3]
-let filePath = "./task.json"
+let command = process.argv[2];
+let arg = process.argv[3];
+let filePath = "./task.json";
 
-const loadTasks = ()=>{
-    try{
-        let bufferData = fs.readFileSync(filePath)
-        let dataJson = bufferData.toString()
-        return JSON.parse(dataJson)
+const loadTasks = () => {
+  try {
+    let bufferData = fs.readFileSync(filePath);
+    let dataJson = bufferData.toString();
+    return JSON.parse(dataJson);
+  } catch {
+    return [];
+  }
+};
+
+const saveTask = (tasks) => {
+  let dataJSON = JSON.stringify(tasks);
+  fs.writeFileSync(filePath, dataJSON);
+};
+
+const addTask = (task) => {
+  let tasks = loadTasks();
+  tasks.push(task);
+  saveTask(tasks);
+};
+
+const listTasks = () => {
+  let tasks = loadTasks();
+  if (tasks.length > 0) {
+    tasks.forEach((task, index) => {
+      console.log(`${index + 1} : ${task}`);
+    });
+  } else {
+    console.log("list is empty :: add atleast one task!");
+  }
+};
+
+const delTask = (index) => {
+  let tasks = loadTasks();
+  let newListTask = tasks.filter((task, i) => {
+    if (i != index) {
+      return task;
     }
-    catch{
-return []
-    }
-}
+  });
+  saveTask(newListTask);
+};
 
-const saveTask = (tasks)=>{
-    let dataJSON = JSON.stringify(tasks)
-    fs.writeFileSync(filePath, dataJSON)
-}
+const delAllTask = () => {
+  let tasks = [];
+  saveTask(tasks);
+};
 
-const addTask = (task)=>{
-    let tasks = loadTasks()
-    tasks.push(task)
-    saveTask(tasks)
-}
-
-const listTasks = ()=>{
-   let tasks =  loadTasks()
-   if(tasks.length > 0) {
-       
-       tasks.forEach((task, index)=>{
-           console.log(`${index+1} : ${task}`)
-        })
-    }
-    else {
-        console.log("list is empty :: add atleast one task!")
-    }
-}
-
-const delTask =(index)=>{
-    let tasks = loadTasks()
-    let newListTask = tasks.filter((task, i) => {
-      if (i != index){
-        return task
-      }
-    })
-    saveTask(newListTask)
-
-}
-
-const delAllTask = () =>{
-    let tasks = []
-    saveTask(tasks)
-}
-
-
-if(command == "add"){
-    addTask(arg)
-}
-else if(command == "list"){
-    listTasks()
-}
-else if(command == "del"){
-    delTask(parseInt(arg)-1)
-}
-
-else if(command == "delAll"){
-    delAllTask()
-}
-
-else {
-    console.log("wrong command input!")
+if (command == "add") {
+  addTask(arg);
+} else if (command == "list") {
+  listTasks();
+} else if (command == "del") {
+  delTask(parseInt(arg) - 1);
+} else if (command == "delAll") {
+  delAllTask();
+} else {
+  console.log("wrong command input!");
 }
