05123959014bf8f52780f5ba58dabebcc7e47417
Shivanshu Koundal <126767653+shivanshuhere@users.noreply.github.com>
2024-10-05 18:54:53 +0530

    add doc load listner

--- JsProjects/todoApp/script.js
+++ JsProjects/todoApp/script.js
@@ -1,45 +1,47 @@
-const taskInput = document.getElementById("taskInput");
-const addBtn = document.getElementById("addBtn");
-const taskList = document.getElementById("taskListContainer");
-const form = document.querySelector("form");
-let taskArr = JSON.parse(localStorage.getItem("tasks")) || [];
+document.addEventListener("DOMContentLoaded", () => {
+  const taskInput = document.getElementById("taskInput");
+  const addBtn = document.getElementById("addBtn");
+  const taskList = document.getElementById("taskListContainer");
+  const form = document.querySelector("form");
+  let taskArr = JSON.parse(localStorage.getItem("tasks")) || [];
 
-function saveTask() {
-  localStorage.setItem("tasks", JSON.stringify(taskArr));
-}
+  function saveTask() {
+    localStorage.setItem("tasks", JSON.stringify(taskArr));
+  }
 
-taskArr.forEach((task) => {
-  taskList.innerHTML += `<div class="task" id=${task.id}>
+  taskArr.forEach((task) => {
+    taskList.innerHTML += `<div class="task" id=${task.id}>
     <p>${task.task}</p>
     <button class="delBtn">Delete</button>
     </div>`;
-});
-
-addBtn.addEventListener("click", () => {
-  let task = taskInput.value;
-  taskInput.value = "";
-  if (task == "") return;
-  //   console.log(task);
-  let id = Date.now();
-  taskArr.push({
-    id: id,
-    task: task,
-    completed: false,
   });
-  saveTask();
 
-  taskList.innerHTML += `<div class="task" id=${id}>
+  addBtn.addEventListener("click", () => {
+    let task = taskInput.value;
+    taskInput.value = "";
+    if (task == "") return;
+    //   console.log(task);
+    let id = Date.now();
+    taskArr.push({
+      id: id,
+      task: task,
+      completed: false,
+    });
+    saveTask();
+
+    taskList.innerHTML += `<div class="task" id=${id}>
             <p>${task}</p>
             <button class="delBtn">Delete</button>
           </div>`;
-});
+  });
 
-document.querySelectorAll("button").forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    e.stopPropagation();
-    const delTask = e.target.parentElement;
-    taskArr = taskArr.filter((task) => task.id != delTask.id);
-    saveTask();
-    delTask.remove();
+  document.querySelectorAll("button").forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.stopPropagation();
+      const delTask = e.target.parentElement;
+      taskArr = taskArr.filter((task) => task.id != delTask.id);
+      saveTask();
+      delTask.remove();
+    });
   });
 });
